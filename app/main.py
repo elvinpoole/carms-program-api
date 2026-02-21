02530291c547ac73212801ac79718ad9c3f13568
elvinpoole <jack.elvinpoole@gmail.com>
2026-02-21 18:48:25 -0500

    added minimal frontend

--- app/main.py
+++ app/main.py
@@ -3,11 +3,16 @@ from sqlalchemy.orm import Session
 from .database import SessionLocal, engine
 from .crud import get_programs, filter_programs_by_province
 from .models import Base
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 # Create tables if they don't exist
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="CaRMS Programs API")
+
+#mount the minimal frontend
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 # Dependency to get DB session
 def get_db():
@@ -20,7 +25,11 @@ def get_db():
 #Title page
 @app.get("/")
 def root():
-    return {"message": "Welcome to the CaRMS Programs API. Use /programs to see data."}
+    return {"message": "Welcome to the CaRMS Programs API. Use /frontend to search the database."}
+
+@app.get("/frontend")
+def frontend():
+    return FileResponse("app/static/index.html")
 
 #List all programs
 @app.get("/programs")
